6428cfb5478d9b98199b3fb0616803a107c1e05b
divyatalwar <divya.talwar@oyorooms.com>
2016-07-31 22:48:20 +0530

    correct indentation

--- scripts/create_visitors.rb
+++ scripts/create_visitors.rb
@@ -1,13 +1,12 @@
 user = User.create({name: 'david', age: 10})
 array = []
 (1..10).each do |i|
-	array << Ticket.create!({ user_id: user.id, fees: 100, event_id: 1})
+  array << Ticket.create!({ user_id: user.id, fees: 100, event_id: 1})
 end
-p array
 (1..20).each do |y|
-	if y < 11
-	  Visitor.create({ticket_id: array[y - 1].id, event_type: 0, timestamp: Time.current - y.hours })
+  if y < 11
+    Visitor.create({ticket_id: array[y - 1].id, event_type: 0, timestamp: Time.current - y.hours })
   else
-	  Visitor.create({ticket_id: array[y - 11].id, event_type: 1, timestamp: Time.current + y.hours })
+    Visitor.create({ticket_id: array[y - 11].id, event_type: 1, timestamp: Time.current + y.hours })
   end
 end
